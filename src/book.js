21d65190248854009340f529a3406e8eb404328c
yangyunhan <1205385190@qq.com>
2018-02-28 13:17:51 +0800

    Fixed

--- src/book.js
+++ src/book.js
@@ -1,38 +1,30 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 
-class Book extends Component{
-
-    render(){
-        return(
-            <ol className="books-grid">
-                {this.props.books.map((book)=>(
-                    <li key={book.id}>
-                        <div className="book">
-                            <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-                                <div className="book-shelf-changer">
-                                    <select value={book.shelf||'none'} onChange={(event)=>this.props.changeShelf(book, event.target.value, this.props.pro)}>
-                                        <option value="none" disabled>Move to...</option>
-                                        <option value="currentlyReading">Currently Reading</option>
-                                        <option value="wantToRead">Want to Read</option>
-                                        <option value="read">Read</option>
-                                        <option value="none">None</option>
-                                    </select>
-                                </div>
+const Book = (props)=>{
+    return(
+        <ol className="books-grid">
+            {props.books.map((book)=>(
+                <li key={book.id}>
+                    <div className="book">
+                        <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                            <div className="book-shelf-changer">
+                                <select value={book.shelf||'none'} onChange={(event)=>props.changeShelf(book, event.target.value)}>
+                                    <option value="null" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                </select>
                             </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors}</div>
                         </div>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
-}
-
-Book.propTypes={
-    books: PropTypes.array.isRequired,
-    changeShelf: PropTypes.func.isRequired
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                    </div>
+                </li>
+            ))}
+        </ol>
+    )
 };
+
 export default Book
