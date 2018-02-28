4924382ff1412441bf74ea753242751dc905a834
yangyunhan <1205385190@qq.com>
2018-02-28 13:19:09 +0800

    Fixed

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -13,12 +13,12 @@ const headers = {
 export const get = (bookId) =>
   fetch(`${api}/books/${bookId}`, { headers })
     .then(res => res.json())
-    .then(data => data.book)
+    .then(data => data.book);
 
 export const getAll = () =>
   fetch(`${api}/books`, { headers })
     .then(res => res.json())
-    .then(data => data.books)
+    .then(data => data.books);
 
 export const update = (book, shelf) =>
   fetch(`${api}/books/${book.id}`, {
@@ -28,7 +28,7 @@ export const update = (book, shelf) =>
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ shelf })
-  }).then(res => res.json())
+  }).then(res => res.json());
 
 export const search = (query) =>
   fetch(`${api}/search`, {
@@ -39,4 +39,4 @@ export const search = (query) =>
     },
     body: JSON.stringify({ query })
   }).then(res => res.json())
-    .then(data => data.books)
+    .then(data => data.books);
--- src/main.js
+++ src/main.js
@@ -1,47 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 import Book from './book'
-import PropTypes from 'prop-types'
 
-class Main extends Component{
-    render(){
-        return(
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Currently Reading</h2>
-                            <div className="bookshelf-books">
-                                <Book books={this.props.book.currentlyReading} changeShelf={this.props.changeEvent} pro={this.props.func}/>
-                            </div>
+const Main = (props)=>{
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Currently Reading</h2>
+                        <div className="bookshelf-books">
+                            <Book books={props.book.currentlyReading} changeShelf={props.changeEvent}/>
                         </div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Want to Read</h2>
-                            <div className="bookshelf-books">
-                                <Book books={this.props.book.wantToRead} changeShelf={this.props.changeEvent} pro={this.props.func}/>
-                            </div>
+                    </div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Want to Read</h2>
+                        <div className="bookshelf-books">
+                            <Book books={props.book.wantToRead} changeShelf={props.changeEvent}/>
                         </div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Read</h2>
-                            <div className="bookshelf-books">
-                                <Book books={this.props.book.read} changeShelf={this.props.changeEvent} pro={this.props.func}/>
-                            </div>
+                    </div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Read</h2>
+                        <div className="bookshelf-books">
+                            <Book books={props.book.read} changeShelf={props.changeEvent}/>
                         </div>
                     </div>
                 </div>
-                <div className="open-search">
-                    <Link to={"/search"}>Add a book</Link>
-                </div>
             </div>
-        )
-    }
-}
-
-Main.propTypes = {
-    book: PropTypes.object.isRequired,
-    changeEvent: PropTypes.func.isRequired
+            <div className="open-search">
+                <Link to={"/search"}>Add a book</Link>
+            </div>
+        </div>
+    )
 };
+
 export default Main
