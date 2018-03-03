adcd95066fea762ed7bd3b125f51434fbce415de
yangyunhan <1205385190@qq.com>
2018-03-03 15:20:29 +0800

    Fix

--- src/Star.js
+++ src/Star.js
@@ -5,36 +5,47 @@ class Star extends Component {
         rating: '0',
     };
 
+    updateStar = (object,value)=>{
+        this.setState({rating: parseInt(value)+1});
 
-    updateStar = (value)=>{
-        this.setState({rating: parseInt(value)+1, open: true});
+        //console.log(value);
+        //console.log(object.target.parentNode);
+
         /*不能在这里用console验证，因为console会先执行，打印出上一次rating的值*/
     };
 
     render(){
+        let className = this.props.renderId;
+        //console.log(className);
+        //console.log(this.state.rating);
         HTMLCollection.prototype.toArray = function () {
             return [].slice.call(this);
         };
 
         let rate = this.state.rating;
-        document.getElementsByClassName('star').toArray().forEach(function (item) {
-            let index = parseInt(item.getAttribute('data-index')) + 1;
-            if (index <= rate){
-                item.setAttribute('class', 'star starStyle')//starStyle是评分的背景色
-            }else{
-                item.setAttribute('class', 'star')
-            }
-            //console.log(item.getAttribute('data-index'));
-        });
+
+        if(this.state.rating > 0 ){
+            let renderNode = document.getElementsByClassName(className);
+            //console.log(renderNode);
+            renderNode[0].children.toArray().forEach(function (item) {
+                //console.log(item);
+                let index = parseInt(item.getAttribute('data-index')) + 1;
+                if (index <= rate){
+                    item.setAttribute('class', 'star starStyle')//starStyle是评分的背景色
+                }else if(item.getAttribute('class') !== 'rate'){
+                    item.setAttribute('class', 'star')
+                }
+            })
+        }
 
         return(
-            <div onClick={(e)=>this.updateStar(e.target.getAttribute('data-index'))}>
+            <div onClick={(e)=>this.updateStar(e, e.target.getAttribute('data-index'))} className={className}>
                 <i data-index="0" className="star"></i>
                 <i data-index="1" className="star"></i>
                 <i data-index="2" className="star"></i>
                 <i data-index="3" className="star"></i>
                 <i data-index="4" className="star"></i>
-                <span className="rate">0</span>
+                <span className="rate">{this.state.rating}</span>
             </div>
         )
     }
