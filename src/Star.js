f82d868618b608fe84dd247f09dd4df10070bf9f
yangyunhan <1205385190@qq.com>
2018-03-04 20:19:53 +0800

    Fix

--- src/Star.js
+++ src/Star.js
@@ -5,47 +5,62 @@ class Star extends Component {
         rating: this.props.storage,
     };
 
-    updateStar = (object,value)=>{
+    /**
+     * description: rate for each book
+     * @param rate
+     * @param div
+     */
+    rating = (rate, div)=>{
+        div.childNodes.forEach(function (item) {
+            let index = parseInt(item.getAttribute('data-index'), 10) + 1;
+            if(index <= rate){
+                item.setAttribute('class', 'star starStyle');
+            }else if(item.getAttribute('class') !== 'rate'){
+                item.setAttribute('class', 'star')
+            }
+        });
+    };
+
+    /**
+     * description: get the score of each book from localStorage
+     */
+    componentDidMount(){
+        let score = this.state.rating;
+        if(score >0 ){
+            this.rating(score, this.refs.renderResult);
+        }
+    }
+
+    /**
+     * description: update the score of each when click the different circles
+     * @param value
+     */
+    updateStar = (value)=>{
         this.setState({rating: parseInt(value)+1});
         /*不能在这里用console验证，因为console会先执行，打印出上一次rating的值*/
+
         let newKey = this.props.renderId.toString();
         localStorage.setItem(newKey, (parseInt(value)+1).toString());
-        console.log(localStorage.getItem(newKey));
     };
 
     render(){
-        //console.log(this.props.storage);
+        let score = this.state.rating;
         let className = this.props.renderId;
-        console.log(className);
-        //console.log(this.state.rating);
-        HTMLCollection.prototype.toArray = function () {
-            return [].slice.call(this);
-        };
-
-        let rate = this.state.rating;
-        console.log(rate);
-        if(this.state.rating > 0 ){
-            let renderNode = document.getElementsByClassName(className);
-            console.log(renderNode);
-            renderNode[0].children.toArray().forEach(function (item) {
-                //console.log(item);
-                let index = parseInt(item.getAttribute('data-index')) + 1;
-                if (index <= rate){
-                    item.setAttribute('class', 'star starStyle')//starStyle是评分的背景色
-                }else if(item.getAttribute('class') !== 'rate'){
-                    item.setAttribute('class', 'star')
-                }
-            })
+        if(score !== this.props.storage){
+            this.rating(score, this.refs.renderResult)
         }
 
         return(
-            <div className={className} onClick={(e)=>this.updateStar(e, e.target.getAttribute('data-index'))}>
+            <div className={className}
+                 ref={"renderResult"}
+                 onClick={(e)=>this.updateStar(e.target.getAttribute('data-index'))}
+            >
                 <i data-index="0" className="star"></i>
                 <i data-index="1" className="star"></i>
                 <i data-index="2" className="star"></i>
                 <i data-index="3" className="star"></i>
                 <i data-index="4" className="star"></i>
-                <span className="rate">{this.state.rating}</span>
+                <span className="rate">{this.state.rating ? this.state.rating:0}</span>
             </div>
         )
     }
